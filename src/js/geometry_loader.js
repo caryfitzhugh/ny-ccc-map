ad14d35460b41519e342b927e6e1361aeba9d4f6
Steve Signell <steve@frontierspatial.com>
2018-12-18 10:35:28 -0500

    changed geometry loaders to https

--- src/js/geometry_loader.js
+++ src/js/geometry_loader.js
@@ -1,10 +1,10 @@
 var GeometryLoader ={
   load: function (name, callback, error_cb) {
     var paths = {
-      "county": CDN("http://api.nescaum-ccsc-dataservices.com/ny.counties.json"),
-      "basin": CDN("http://api.nescaum-ccsc-dataservices.com/ny.basins.topojson"),
-      "watershed": CDN("http://repository.nescaum-ccsc-dataservices.com/data/ny/watershed.json"),
-      "state": CDN("http://repository.nescaum-ccsc-dataservices.com/data/ny/state.topojson")
+      "county": CDN("https://api.nescaum-ccsc-dataservices.com/ny.counties.json"),
+      "basin": CDN("https://api.nescaum-ccsc-dataservices.com/ny.basins.topojson"),
+      "watershed": CDN("https://repository.nescaum-ccsc-dataservices.com/data/ny/watershed.json"),
+      "state": CDN("https://repository.nescaum-ccsc-dataservices.com/data/ny/state.topojson")
     };
 
     let return_val = () => {
